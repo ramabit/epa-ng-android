135dba2f190d0ed280960185098825f90101a7d2
Fernando Ramirez <f.e.ramirez94@gmail.com>
2017-11-17 13:04:35 +0100

    add layout components without behaviour

--- app/src/main/java/org/hits/epa_ng_android/MainActivity.java
+++ app/src/main/java/org/hits/epa_ng_android/MainActivity.java
@@ -5,24 +5,59 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.mainLoadingTreesProgressBar)
+    ProgressBar loadingTreesProgressBar;
+
+    @BindView(R.id.mainLoadingTreesTextView)
+    TextView loadingTreesTextView;
+
+    @BindView(R.id.mainTreesListSpinner)
+    Spinner treesListSpinner;
+
+    @BindView(R.id.mainSelectQSFileButton)
+    Button selectQSFileButton;
+
+    @BindView(R.id.mainSelectedQSFileNameTextView)
+    TextView selectedQSFileNameTextView;
+
+    @BindView(R.id.mainUploadQSFileButton)
+    Button uploadQSFileButton;
+
+    @BindView(R.id.mainUploadedQSFileTextView)
+    TextView uploadedQSFileTextView;
+
+    @BindView(R.id.mainSendRequestFloatingButton)
+    FloatingActionButton sendRequestFloatingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        ButterKnife.bind(this);
+
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        sendRequestFloatingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Here I will send the request", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
