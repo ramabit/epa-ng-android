761ea8a8f949ccaa52a2ae686b999800e5aa006c
Fernando Ramirez <f.e.ramirez94@gmail.com>
2017-11-20 17:00:07 +0100

    refactoring and improve ui

--- app/src/main/java/org/hits/epa_ng_android/activities/MainActivity.java
+++ app/src/main/java/org/hits/epa_ng_android/activities/MainActivity.java
@@ -1,17 +1,8 @@
 package org.hits.epa_ng_android.activities;
 
-import android.content.ContentUris;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
-import android.provider.OpenableColumns;
-import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -32,6 +23,7 @@ import org.hits.epa_ng_android.models.QSFile;
 import org.hits.epa_ng_android.models.responses.QSFileUploadResponse;
 import org.hits.epa_ng_android.models.responses.TreesResponse;
 import org.hits.epa_ng_android.network.EPAngServiceAPI;
+import org.hits.epa_ng_android.utils.FileManager;
 
 import java.io.File;
 import java.util.List;
@@ -123,30 +115,13 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case CHOOSE_FILE_CODE:
                 if (resultCode == RESULT_OK) {
-                    // Get the Uri of the selected file
                     Uri uri = data.getData();
+                    String uriString = uri.toString();
 
-                    String fullPath = getPath(this, uri);
+                    String fullPath = FileManager.getPath(this, uri);
                     File file = new File(fullPath);
 
-                    String uriString = uri.toString();
-//                    File file = new File(getRealPathFromURI(uri));
-
-                    String displayName = null;
-
-                    if (uriString.startsWith("content://")) {
-                        Cursor cursor = null;
-                        try {
-                            cursor = getContentResolver().query(uri, null, null, null, null);
-                            if (cursor != null && cursor.moveToFirst()) {
-                                displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
-                            }
-                        } finally {
-                            cursor.close();
-                        }
-                    } else if (uriString.startsWith("file://")) {
-                        displayName = file.getName();
-                    }
+                    String displayName = FileManager.getFileDisplayName(this, uri, file, uriString);
 
                     QSFile qsFile = new QSFile(file, displayName, uri);
                     qsFileSelected(qsFile);
@@ -156,6 +131,7 @@ public class MainActivity extends AppCompatActivity {
 
         super.onActivityResult(requestCode, resultCode, data);
     }
+
 
     public void getSupportedTrees() {
         treesListSpinner.setEnabled(false);
@@ -229,9 +205,11 @@ public class MainActivity extends AppCompatActivity {
         mAttachedFile = null;
         selectedQSFileNameTextView.setVisibility(View.INVISIBLE);
         uploadQSFileButton.setEnabled(false);
+        noQSFileUploaded();
     }
 
     private void qsFileUploaded() {
+        Toast.makeText(this, "Query Sequence file uploaded successfully!", Toast.LENGTH_SHORT).show();
         uploadQSFileButton.setVisibility(View.INVISIBLE);
         uploadedQSFileTextView.setVisibility(View.VISIBLE);
         sendRequestFloatingButton.setVisibility(View.VISIBLE);
@@ -260,6 +238,7 @@ public class MainActivity extends AppCompatActivity {
                 QSFileUploadResponse qsFileUploadResponse = response.body();
                 if (qsFileUploadResponse != null) {
                     mAttachedFile.setToken(qsFileUploadResponse.getToken());
+                    qsFileUploaded();
                 }
             }
 
@@ -268,98 +247,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.d("Upload QS file", t.getMessage());
             }
         });
-    }
-
-    @Nullable
-    public static String getPath(Context context, Uri uri) {
-        // DocumentProvider
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(context, uri)) {
-            // ExternalStorageProvider
-            if (isExternalStorageDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                if ("primary".equalsIgnoreCase(type)) {
-                    return Environment.getExternalStorageDirectory() + "/" + split[1];
-                }
-            } else if (isDownloadsDocument(uri)) {// DownloadsProvider
-                final String id = DocumentsContract.getDocumentId(uri);
-                final Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-                return getDataColumn(context, contentUri, null, null);
-
-            } else if (isMediaDocument(uri)) { // MediaProvider
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-                Uri contentUri = null;
-                if ("image".equals(type)) {
-                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                } else if ("video".equals(type)) {
-                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                } else if ("audio".equals(type)) {
-                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                }
-                final String selection = "_id=?";
-                final String[] selectionArgs = new String[]{split[1]};
-                return getDataColumn(context, contentUri, selection, selectionArgs);
-
-            }
-        } else if ("content".equalsIgnoreCase(uri.getScheme())) {// MediaStore (and general)
-            // Return the remote address
-            if (isGooglePhotosUri(uri))
-                return uri.getLastPathSegment();
-            return getDataColumn(context, uri, null, null);
-
-        } else if ("file".equalsIgnoreCase(uri.getScheme())) {// File
-            return uri.getPath();
-        }
-        return null;
-    }
-
-    public static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
-        Cursor cursor = null;
-        final String column = "_data";
-        final String[] projection = {column};
-        try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
-            if (cursor != null && cursor.moveToFirst()) {
-                final int index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(index);
-            }
-        } finally {
-            if (cursor != null)
-                cursor.close();
-        }
-        return null;
-    }
-
-    public static boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is DownloadsProvider.
-     */
-    public static boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is MediaProvider.
-     */
-    public static boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
-    }
-
-    /**
-     * @param uri The Uri to check.
-     * @return Whether the Uri authority is Google Photos.
-     */
-    public static boolean isGooglePhotosUri(Uri uri) {
-        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
     }
 
 }
